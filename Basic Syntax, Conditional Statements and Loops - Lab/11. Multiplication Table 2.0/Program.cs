7252194b7293addfc52dd0c84c641426afdccc07
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-06 19:32:07 +0300

    Add files via upload

--- Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication Table 2.0/Program.cs
+++ Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication Table 2.0/Program.cs
@@ -1,1 +1,35 @@
+﻿using System;
 
+namespace _11._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int theInteger = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+
+            if (times <= 10)
+            {
+
+                for (int i = times; i <= 10; i++)
+                {
+                   
+                    {
+                        Console.WriteLine($"{theInteger} X {times} = {theInteger * times}");
+                        times++;
+                    }
+                    
+
+                }
+            }
+            else 
+            {
+                Console.WriteLine($"{theInteger} X {times} = {theInteger * times}");
+            }
+
+
+        }
+    }
+}
