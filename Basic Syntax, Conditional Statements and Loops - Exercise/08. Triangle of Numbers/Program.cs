761c94edc4e63b39e4a890a3927367958816e972
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-08 20:08:08 +0300

    Add files via upload

--- Basic Syntax, Conditional Statements and Loops - Exercise/08. Triangle of Numbers/Program.cs
+++ Basic Syntax, Conditional Statements and Loops - Exercise/08. Triangle of Numbers/Program.cs
@@ -1,1 +1,20 @@
+﻿using System;
 
+namespace _08._Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
