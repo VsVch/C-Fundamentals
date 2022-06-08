dd179512035c12e47fc0fe4224a151318824f4e9
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-08 20:10:36 +0300

    Add files via upload

--- Basic Syntax, Conditional Statements and Loops - Exercise/10. Rage Expenses/Program.cs
+++ Basic Syntax, Conditional Statements and Loops - Exercise/10. Rage Expenses/Program.cs
@@ -1,1 +1,28 @@
+﻿using System;
 
+namespace _10._Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double priceHeadset = double.Parse(Console.ReadLine());
+            double priceMouse = double.Parse(Console.ReadLine());
+            double priceKeybord = double.Parse(Console.ReadLine());
+            double priceDisplay = double.Parse(Console.ReadLine());
+
+            int trashedHeadset = lostGames / 2;
+            int trashedMouse = lostGames / 3;
+            int trashedKeyboad = lostGames / 6;
+            int trashedDisplay = lostGames / 12;
+
+            double sum = priceHeadset * trashedHeadset + priceMouse * trashedMouse + priceKeybord * trashedKeyboad + priceDisplay * trashedDisplay;
+            Console.WriteLine($"Rage expenses: {sum:f2} lv.");
+
+
+
+
+        }
+    }
+}
