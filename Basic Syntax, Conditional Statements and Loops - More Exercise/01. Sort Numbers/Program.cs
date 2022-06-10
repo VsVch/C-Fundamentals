73068988f25d4eeaf5f3777c4dd3bfaf465bf2f6
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-10 19:37:11 +0300

    Add files via upload

--- Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/Program.cs
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/Program.cs
@@ -1,1 +1,61 @@
+﻿using System;
 
+namespace _01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            if (first > second && first > third)
+            {
+                Console.WriteLine(first);
+                if (second > third)
+                {
+                    Console.WriteLine(second);
+                    Console.WriteLine(third);
+                }
+                else
+                {
+                    Console.WriteLine(third);
+                    Console.WriteLine(second);
+                }
+            }
+            if (second > third && second > first)
+            {
+                Console.WriteLine(second);
+                if (third > first)
+                {
+                    Console.WriteLine(third);
+                    Console.WriteLine(first);
+                }
+                else
+                {
+                    Console.WriteLine(first);
+                    Console.WriteLine(third);
+                }
+            }
+            if (third > first && third > second)
+            {
+                Console.WriteLine(third);
+                if (first > second)
+                {
+                    Console.WriteLine(first);
+                    Console.WriteLine(second);
+                }
+                else
+                {
+                    Console.WriteLine(second);
+                    Console.WriteLine(first);
+                }
+            }
+
+
+
+
+        }
+    }
+}
