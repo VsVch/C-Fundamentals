099425ea4c28a486a9674d11e4fd1e70ed5ac241
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-13 18:45:18 +0300

    Add files via upload

--- Data Types and Variables - Exercise/03. Elevator/Program.cs
+++ Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -1,1 +1,32 @@
+﻿using System;
 
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int count = numberPeople / capacity;
+            if (numberPeople % capacity  != 0)
+            {
+                count++;
+                Console.WriteLine(count);
+            }
+            else
+            {
+                Console.WriteLine(count);
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
