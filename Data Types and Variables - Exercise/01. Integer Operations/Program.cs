f43919f521b20325bab566ba71f47638ae992f6c
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-13 18:43:32 +0300

    Add files via upload

--- Data Types and Variables - Exercise/01. Integer Operations/Program.cs
+++ Data Types and Variables - Exercise/01. Integer Operations/Program.cs
@@ -1,1 +1,27 @@
+﻿using System;
 
+namespace _01._Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int first = int.Parse(Console.ReadLine());
+            int sec = int.Parse(Console.ReadLine());
+            int th = int.Parse(Console.ReadLine());
+            int four = int.Parse(Console.ReadLine());
+
+            int sumFirstSecond = first + sec;
+            int divideThSum = sumFirstSecond / th;
+            int multiply = four * divideThSum;
+
+            Console.WriteLine(multiply);
+
+
+
+
+
+        }
+    }
+}
