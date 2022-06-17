1d41e27633b8c861039da1ae62137b075a975b98
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-17 06:47:26 +0300

    Add files via upload

--- Arrays - Lab/04. Reverse Array of Strings/Program.cs
+++ Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -1,1 +1,22 @@
+﻿using System;
+using System.Linq;
 
+namespace _07.IdenticalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').ToArray();
+            var arr = input;
+            var reverse = arr.Reverse();
+            Console.WriteLine(string.Join(" ", arr.Reverse()));
+
+
+
+
+
+
+        }
+    }
+}
