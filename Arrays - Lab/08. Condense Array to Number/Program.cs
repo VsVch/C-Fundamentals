862f52e2e4997d8569196e66aeecbdd87bfcedd8
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-17 06:51:25 +0300

    Add files via upload

--- Arrays - Lab/08. Condense Array to Number/Program.cs
+++ Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -1,1 +1,47 @@
+﻿using System;
+using System.Linq;
 
+
+namespace _08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var inputArr = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var count = inputArr.Length;
+            int sum = 0;
+            if (inputArr.Length == 1)
+            {
+                sum = inputArr[0];
+                Console.WriteLine(sum);
+                return;
+
+            }
+            else
+            {
+                while (count >= 0)
+                {
+                    for (int i = 0; i < count - 1; i++)
+                    {
+                        inputArr[i] = inputArr[i] + inputArr[i + 1];
+                        //Console.Write("{0}{1}", inputArr[i], i == count - 2 ? string.Empty : " ");
+                        sum = inputArr[i];
+
+                    }
+
+
+                    count--;
+                   
+                }
+                Console.WriteLine(sum);
+                return;
+            }
+            
+            
+        }
+    }
+}
