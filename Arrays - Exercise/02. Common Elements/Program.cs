1e10f5906e59ca85a6cd953bd0f5be2deb5b3ca0
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-18 11:07:34 +0300

    Add files via upload

--- Arrays - Exercise/02. Common Elements/Program.cs
+++ Arrays - Exercise/02. Common Elements/Program.cs
@@ -1,1 +1,37 @@
+﻿using System;
 
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arrOne = Console.ReadLine().Split(" ");
+            string[] arrTwo = Console.ReadLine().Split(" ");
+            
+            for (int j = 0; j < arrTwo.Length; j++)
+            {
+                for (int i = arrOne.Length-1; i >= 0; i--)
+                {
+                    if (arrOne[i] == arrTwo[j])
+                    {
+                        Console.Write($"{arrTwo[j]} ");
+                        break;
+                    }
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
