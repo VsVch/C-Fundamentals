0f23e99051140774ad8057d4369c7470ec1bb340
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-18 11:08:23 +0300

    Add files via upload

--- Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
+++ Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -1,1 +1,39 @@
+﻿using System;
+using System.Linq;
 
+namespace _03._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arrOdd = new int[n];
+            int[] arrEven = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (i % 2 == 0)
+                {
+                    arrEven[i] = numbers[1];
+                    arrOdd[i] = numbers[0];
+                    
+                    
+                }
+                else
+                {
+                    
+                    arrEven[i] = numbers[0];
+                    arrOdd[i] = numbers[1];
+
+
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", arrOdd));
+            Console.WriteLine(string.Join(" ", arrEven));
+            
+        }
+    }
+}
