6fbf64c63c31de39a497fc03c77bb4f253d4df5d
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-18 11:13:23 +0300

    Add files via upload

--- Arrays - Exercise/08. Magic Sum/Program.cs
+++ Arrays - Exercise/08. Magic Sum/Program.cs
@@ -1,1 +1,37 @@
+﻿using System;
+using System.Linq;
 
+namespace _08_MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+
+                for (int j = arr.Length-1; j >i ; j--)
+                {
+                    if ((arr[i] + arr[j]) == number)
+                    {
+                        Console.Write($"{arr[i]} {arr[j]}");
+                        Console.WriteLine();
+                    }
+                }
+                
+               
+            }
+
+
+
+
+
+
+        }
+    }
+}
