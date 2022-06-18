68a057eeec850658c4c0311b549f59c23f2d6b7e
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-18 11:09:33 +0300

    Add files via upload

--- Arrays - Exercise/04. Array Rotation/Program.cs
+++ Arrays - Exercise/04. Array Rotation/Program.cs
@@ -1,1 +1,34 @@
+﻿using System;
+using System.Linq;
 
+namespace _04._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int posisionRotation = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < posisionRotation; i++)
+            {
+                int temp = arr[0];
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.Length - 1] = temp;
+
+            }
+
+
+
+            Console.WriteLine(string.Join(" ", arr ));
+
+
+
+
+        }
+    }
+}
