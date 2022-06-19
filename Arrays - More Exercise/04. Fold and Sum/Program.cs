1a789b1a175b7000eb78fbccc73ed2816bd6dc5f
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-19 09:51:09 +0300

    Add files via upload

--- Arrays - More Exercise/04. Fold and Sum/Program.cs
+++ Arrays - More Exercise/04. Fold and Sum/Program.cs
@@ -1,1 +1,42 @@
+﻿using System;
+using System.Linq;
+namespace Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+            int indexLeft = arr.Length / 4 - 1;
+            int indexRight = 3 * arr.Length / 4;
+
+            int[] middle = new int[arr.Length/2];
+            int[] ends = new int[arr.Length/2];
+            int count = 0;
+
+            for (int i = indexLeft; i >= 0; i--)
+            {
+                ends[indexLeft - i] = arr[i];
+                count++;
+            }
+
+            for (int j = arr.Length-1; j >= indexRight; j--)
+            {
+                ends[arr.Length - 1 - j + count] = arr[j];
+            }
+
+            for (int k = indexLeft + 1; k < indexRight; k++)
+            {
+                middle[k - count] = arr[k];
+            }
+
+            for (int m = 0; m < middle.Length; m++)
+            {
+                Console.Write(middle[m] + ends[m] + " ");
+            }
+
+
+        }
+    }
+}
