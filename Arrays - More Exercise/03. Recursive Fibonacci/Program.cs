18c5ae33fa56e9b771925c7945db79264846aa0e
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-19 09:50:31 +0300

    Add files via upload

--- Arrays - More Exercise/03. Recursive Fibonacci/Program.cs
+++ Arrays - More Exercise/03. Recursive Fibonacci/Program.cs
@@ -1,1 +1,25 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        int[] fib = new int[50];
+        fib[0] = 1;
+        fib[1] = 1;
+        if (num > 2)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i-2];
 
+            }
+
+
+
+        }
+
+        Console.WriteLine(fib[num-1]);
+    }
+}
