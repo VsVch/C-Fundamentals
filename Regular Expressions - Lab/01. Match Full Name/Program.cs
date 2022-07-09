8aa546e5cbf6d9567a57417c263907da64ada2a5
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-07-09 13:36:38 +0300

    Add files via upload

--- Regular Expressions - Lab/01. Match Full Name/Program.cs
+++ Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -1,1 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            Regex patten = new Regex(regex);
+            MatchCollection mathe = patten.Matches(input);
+            //List result = new List(mathe);
+            foreach (Match item in mathe)
+            {
+                Console.Write(item.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
